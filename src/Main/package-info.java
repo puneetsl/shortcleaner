9b3833592c1f92705a2e2b0d32b208d34bc1b83b
puneetsl <everlasting.puneet@gmail.com>
2014-02-20 23:22:20 -0500

    Defined Packages

---
+++ src/Main/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Package for unit testing my code, not using jUnit at this time.
+ * TODO use jUnit in future
+ */
+/**
+ * @author puneet
+ * @version 0.1
+ */
+package Main;
---
+++ src/psl/shortcleaner/utils/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Package contains specific utilities
+ */
+/**
+ * @author puneet
+ * @version 0.1
+ */
+package psl.shortcleaner.utils;
