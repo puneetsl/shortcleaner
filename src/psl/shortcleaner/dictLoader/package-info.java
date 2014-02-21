ec92fc1a7ee9c228683f576b7d40ba84a815f5d8
puneetsl <everlasting.puneet@gmail.com>
2014-02-21 03:17:00 -0500

    Added a dictionary and package info for new packages

---
+++ src/psl/shortcleaner/dictLoader/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * All classes must follow singleton pattern or must provide loaded dictionaries.
+ * Dictionaries must be loaded only once
+ */
+/**
+ * @author puneet
+ * @version 0.1
+ */
+package psl.shortcleaner.dictLoader;
---
+++ src/psl/shortcleaner/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * this is the root package
+ */
+/**
+ * 	@author puneet
+ *	@version 0.1
+ */
+package psl.shortcleaner;
