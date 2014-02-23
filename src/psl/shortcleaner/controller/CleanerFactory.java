87b8a2b62a05cbd3ac0987f7d3ee8a04008dba2b
puneetsl <everlasting.puneet@gmail.com>
2014-02-23 01:32:16 -0500

    Commit before sleeping

---
+++ src/psl/shortcleaner/controller/CleanerFactory.java
@@ -0,0 +1,15 @@
+/**
+ * Creates cleaner object and gives it to controller 
+ * using property and reflections
+ * 
+ */
+package psl.shortcleaner.controller;
+
+/**
+ * @author puneet
+ * @version 0.1
+ * 
+ */
+public class CleanerFactory {
+
+}
