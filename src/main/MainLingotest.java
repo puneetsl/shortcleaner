8ff29fd8beacc470828029095670f2356aef9430
puneetsl <everlasting.puneet@gmail.com>
2014-03-16 01:32:00 -0400

    Adding dictionary

---
+++ src/main/MainLingotest.java
@@ -0,0 +1,19 @@
+package main;
+
+
+import psl.shortcleaner.utils.StringUtils;
+import psl.shortcleaner.dictLoader.TwitterAbbreviationDictionary;
+import psl.shortcleaner.tokenizer.SimpleTokenizer;
+
+public class MainLingotest {
+	public static void main(String[] args){
+		TwitterAbbreviationDictionary tad = new TwitterAbbreviationDictionary();
+		String a = "Lol omg this is so funny lmao";
+		String b[] = SimpleTokenizer.tokenize(a);
+		for (int i=0;i<b.length;i++) {
+			b[i]=tad.getDictionaryValue(b[i]);
+		}
+		System.out.println(StringUtils.join(b," "));
+
+	}
+}
